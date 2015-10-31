9eb893a62d9ec84f9453c77220b48823f2a0bfb9
rheitkamp <rae.heitkamp@gmail.com>
2015-10-31 09:01:41 -0400

    Initialize script for planet rotation on x and z axis
    Also added texture to planet to visualize rotation better.

---
+++ Leaf-sheep/Assets/Scripts/RotatePlanet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatePlanet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+    // get reference to x axis
+    // get reference to z axis
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+    // if pressing left or right arrow keys, rotate on z axis
+    // if pressing up or down arrow keys, rotate on x axis
+
+    // also need to add code for rotating sheep toward direction of motion
+
+	}
+}
