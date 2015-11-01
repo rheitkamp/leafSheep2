35002066f2b98faf1e5d37cfb869103fbbc8f030
rheitkamp <rae.heitkamp@gmail.com>
2015-10-31 22:35:05 -0400

    Rotate planet in world space, not local
    Also adds placeholder for leaf sheep

--- Leaf-sheep/Assets/Scripts/RotatePlanet.cs
+++ Leaf-sheep/Assets/Scripts/RotatePlanet.cs
@@ -5,25 +5,17 @@ public class RotatePlanet : MonoBehaviour {
 
 	public float rotationSpeed = 3f; 
 
-	// Use this for initialization
-	void Start () {
-	
-    // get reference to x axis
-    // get reference to z axis
-
-	}
-	
 	// Update is called once per frame
 	void Update () {
+		// rotate leaf sheep toward opposite of direction of motion
+		// when leaf sheep is facing direction of motion, move the planet beneath
 
-		float rollForwardsOrBackwards = -Input.GetAxis ("Vertical") * rotationSpeed;
-		float rollLeftOrRight = -Input.GetAxis ("Horizontal") * rotationSpeed;
+			// if pressing up or down arrow keys, rotate on x axis
+			float rollForwardsOrBackwards = -Input.GetAxis ("Vertical") * rotationSpeed;
+			// if pressing left or right arrow keys, rotate on z axis
+			float rollLeftOrRight = -Input.GetAxis ("Horizontal") * rotationSpeed;
 
-		transform.Rotate (rollLeftOrRight, 0, rollForwardsOrBackwards);
-    // if pressing left or right arrow keys, rotate on z axis
-    // if pressing up or down arrow keys, rotate on x axis
-
-    // also need to add code for rotating sheep toward direction of motion
+			transform.Rotate (rollLeftOrRight, 0, rollForwardsOrBackwards, Space.World);
 
 	}
 }
