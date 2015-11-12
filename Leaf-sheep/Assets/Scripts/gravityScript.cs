3647ba527020a70a8f6ad052a8d72e56fe86428f
rheitkamp <rae.heitkamp@gmail.com>
2015-11-11 19:57:42 -0500

    Adds gravity script to dead algae

---
+++ Leaf-sheep/Assets/Scripts/gravityScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gravityScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 gravityOrigin = new Vector3 (0f, 0f, 0f);
+
+		Vector3 toGravityOriginFromObject = gravityOrigin - gameObject.transform.position;
+		toGravityOriginFromObject.Normalize ();
+
+		float accelerationDueToGravity = 9.8f;
+		toGravityOriginFromObject *= accelerationDueToGravity * gameObject.GetComponent<Rigidbody>().mass * Time.deltaTime;
+
+		gameObject.GetComponent<Rigidbody>().AddForce (toGravityOriginFromObject, ForceMode.Acceleration);
+
+	}
+}
