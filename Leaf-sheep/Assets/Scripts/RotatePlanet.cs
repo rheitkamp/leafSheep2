48acd93ef0783df76b1355d604eccac1a7ef32cf
rheitkamp <rae.heitkamp@gmail.com>
2015-11-14 17:31:36 -0500

    Changes absolute transform on planet to Add Force

--- Leaf-sheep/Assets/Scripts/RotatePlanet.cs
+++ Leaf-sheep/Assets/Scripts/RotatePlanet.cs
@@ -3,19 +3,30 @@ using System.Collections;
 
 public class RotatePlanet : MonoBehaviour {
 
-	public float rotationSpeed = 3f; 
+	public float rotationSpeed = 3f;
+    public Rigidbody rb; 
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
 	// Update is called once per frame
 	void Update () {
 		// rotate leaf sheep toward opposite of direction of motion
 		// when leaf sheep is facing direction of motion, move the planet beneath
 
+
 			// if pressing up or down arrow keys, rotate on x axis
 			float rollForwardsOrBackwards = -Input.GetAxis ("Vertical") * rotationSpeed;
 			// if pressing left or right arrow keys, rotate on z axis
 			float rollLeftOrRight = -Input.GetAxis ("Horizontal") * rotationSpeed;
 
-			transform.Rotate (rollLeftOrRight, 0, rollForwardsOrBackwards, Space.World);
-
-	}
+        //in order for collisions to be detected correctly, movement has to be dependent on force/torque added, not position change
+        rb.AddTorque(rollLeftOrRight, rollForwardsOrBackwards, 0f);
+        /*
+        transform.Rotate (rollLeftOrRight, 0, rollForwardsOrBackwards, Space.World);
+        */
+    }
 }
